10cc33252ebf73bc93aaa1f96f8dc8bd9a9d72fb
Manikanta <chandamanikanta7@gmail.com>
2026-06-09 18:43:31 +0530

    add frontend deployed url in cors

--- spring-backend/backend/src/main/java/com/eventhub/backend/security/SecurityConfig.java
+++ spring-backend/backend/src/main/java/com/eventhub/backend/security/SecurityConfig.java
@@ -77,7 +77,8 @@ public class SecurityConfig {
                                 "http://localhost:3000",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:3000",
-                                "http://127.0.0.1:5173"));
+                                "http://127.0.0.1:5173",
+                                "https://rezervo-booking.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList(
                                 "GET",
                                 "POST",
